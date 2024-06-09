e95c58cfb3b412ddc704b82927ee7f41017bd319
daksh123456 <dakshkanaujia@gmail.com>
2024-06-09 18:26:31 +0530

    to run

--- src/App.jsx
+++ src/App.jsx
@@ -7,23 +7,16 @@ import { Routes, Route } from 'react-router-dom';
 import About from './Components/About/About.jsx'
 
 import './Common.css'
-const[defaultC, setDefault] = (1);
 function App() {
-
-function change(e){
-  const value = e.target;
-  console.log(value);
-    setDefault(value);
-}
   return (
     <>
       <div className='Navbar'>
-        <Navbar value = {defaultC} font={"poppins"} textColor={"white"}/>
+        <Navbar font={"poppins"} textColor={"white"}/>
       </div>
       <Routes>
-        <Route onclick={change} value={1} path="/" element={<Homepage />} />
-        <Route onclick={change} value={2} path="/about" element={<About />} />
-        <Route onclick={change} value={3} path="/contact" element={<Contact />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,51 +1,25 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Navbar.css';
 import '../../Common.css'
 import { Link } from 'react-router-dom';
 
-// Mapping values to class names
-const valueMap = {
-  1: "home",
-  2: "about",
-  3: "contact",
-  4: "projects"
-};
 
-function underline(value) {
-  const classValue = valueMap[value];
-  const navElement = document.querySelector(`.${classValue}`);
-  if (navElement) {
-    navElement.style.borderBottom = '2px solid black'; // Apply underline style
-  }
+function Navbar({font, textColor}) {
+
+    return (
+      <>
+        <div className='MainDiv'>
+          <div className="name poppins">Daksh Kanaujia</div>
+          <nav>
+              <ul className='navbar'>
+                  <li className={`nav-item text-${textColor} ${font} home`}><Link to = '/'>Home</Link></li>
+                  <li className={`nav-item text-${textColor} ${font} about`}><Link to = '/about'>About</Link></li>
+                  <li className={`nav-item text-${textColor} ${font} contact`}><Link to = '/contact'>Contact</Link></li>
+                  <li className={`nav-item text-${textColor} ${font} projects`}><Link to = '/projects'>Projects</Link></li>
+              </ul>
+          </nav>
+        </div>
+      </>
+    )
 }
-
-function Navbar({ value1, font, textColor }) {
-  // Use effect to call underline when the component mounts and value1 changes
-  useEffect(() => {
-    underline(value1);
-  }, [value1]);
-
-  return (
-    <div className='MainDiv'>
-      <div className="name poppins">Daksh Kanaujia</div>
-      <nav>
-        <ul className='navbar'>
-          <li className={`nav-item text-${textColor} ${font} home`}>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className={`nav-item text-${textColor} ${font} about`}>
-            <Link to='/about'>About</Link>
-          </li>
-          <li className={`nav-item text-${textColor} ${font} contact`}>
-            <Link to='/contact'>Contact</Link>
-          </li>
-          <li className={`nav-item text-${textColor} ${font} projects`}>
-            <Link to='/projects'>Projects</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
-
 export default Navbar;
