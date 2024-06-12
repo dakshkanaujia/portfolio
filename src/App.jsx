bf0971ad3c0668d4752ec8a47d1c66e78c916f77
daksh123456 <dakshkanaujia@gmail.com>
2024-06-12 21:08:47 +0530

    Page Reload Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,27 @@ import './App.css'
 import Homepage from './Components/HomePage/homepage.jsx'
 import Navbar from './Components/Navbar/Navbar.jsx'
 import Contact from './Components/Contact/Contact.jsx'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import About from './Components/About/About.jsx'
 import Projects from './Components/Projects/Projects.jsx'
-
 import './Common.css'
+
 function App() {
   return (
-    <>
-      <div className='Navbar'>
-        <Navbar font={"poppins"} textColor={"white"}/>
+    <Router>
+      <div>
+        <div className='Navbar'>
+          <Navbar font={"poppins"} textColor={"white"}/>
+        </div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </>
+    </Router>
   )
 }
-
-
 
 export default App
